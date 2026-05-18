4119562325f112dacd58ad6ad3e46b71536591b1
dzidula <gamathodzidula@gmail.com>
2026-05-18 10:39:44 +0000

    Fix(stats) : Suppression des champs pour l'enregistrement inutile de certaines colonnes

--- libs/models/portfolio/Stats.ts
+++ libs/models/portfolio/Stats.ts
@@ -4,20 +4,5 @@ export const StatsSchema = new mongoose.Schema({
         careerStartDate: {
                 type: Date,
                 default: null,
-        },
-
-        experienceYears: {
-                type: Number,
-                default: 0,
-        },
-
-        projectsCount: {
-                type: Number,
-                default: 0,
-        },
-
-        clientsSatisfied: {
-                type: Number,
-                default: 0,
-        },
+        }
 });
